228d250dd14f5fb1a8608940c83adb489b9181f8
shahedyousuf <smshahed.yousuf@gmail.com>
2022-05-28 14:47:35 +0200

    getting all parts

--- index.js
+++ index.js
@@ -8,10 +8,33 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qhu2zmw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const partsCollection = client.db('camera-parts-manufacturer').collection('parts');
+
+        app.get('/parts', async (req, res) => {
+            const query = {};
+            const parts = await partsCollection.find(query).toArray();
+            res.send(parts);
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
     res.send('Server is running');
 })
 
 app.listen(port, () => {
-    console.log(`Server is running on ${port}`);
+    console.log(`Server is running on port ${port}`);
 })
