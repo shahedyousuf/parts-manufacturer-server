6946f86f8438871e5d7979cb1fd835b4d1eb313a
shahedyousuf <smshahed.yousuf@gmail.com>
2022-05-29 11:15:58 +0200

    all orders

--- index.js
+++ index.js
@@ -63,6 +63,11 @@ async function run() {
 
         })
 
+        app.get('/order', async (req, res) => {
+            const orders = await orderCollection.find().toArray();
+            res.send(orders);
+        })
+
         app.post('/order', async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
